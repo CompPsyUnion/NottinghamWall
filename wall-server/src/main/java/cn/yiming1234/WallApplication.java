14d0b1f9275f71fe8ca4cbce7a46c4d1bc72d66b
自由的世界人 <3196812536@qq.com>
2024-06-15 01:36:05 +0800

    创建初始工程

---
+++ wall-server/src/main/java/cn/yiming1234/WallApplication.java
@@ -0,0 +1,16 @@
+package cn.yiming1234;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement //开启注解方式的事务管理
+@Slf4j
+public class WallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WallApplication.class, args);
+        log.info("server started");
+    }
+}
